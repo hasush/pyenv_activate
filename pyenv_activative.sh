cf03235fb68527580965ca34ef590f143caf80f1
Yash Sandhu <hasush@gmail.com>
2024-12-16 23:14:53 -0500

    Update pyenv_activative.sh

--- pyenv_activative.sh
+++ pyenv_activative.sh
@@ -1,4 +1,4 @@
 pyenv install 3.9.18
-pyenv virtualenv 3.9.18 clip-embedding-tagger
-pyenv activate clip-embedding-tagger
+pyenv virtualenv 3.9.18 tagger
+pyenv activate tagger
 pip install -r requirements.txt
